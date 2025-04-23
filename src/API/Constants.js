0a9df0fa706d9a9df47975eb13b8a2cd4e3d3723
mani8251 <manikanta995975@gmail.com>
2025-04-23 21:49:18 +0530

    first

--- src/API/Constants.js
+++ src/API/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000/"
+export const BASE_URL = "https://school-backend-3-2w8s.onrender.com/"
 
 export const STATUS_CODE = {
   INTERNAL_SERVER_ERROR: 500,
